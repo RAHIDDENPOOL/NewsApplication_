60c91bca02ef0a32342eab7d65b4a233e54f4024
HIDDENPOOL <kazahstanuzbekskij@gmail.com>
2022-10-25 23:24:51 +0300

    UPD 1.4

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "f5e191662a3744cdbe19927b5f78899f"
+    }
+}
