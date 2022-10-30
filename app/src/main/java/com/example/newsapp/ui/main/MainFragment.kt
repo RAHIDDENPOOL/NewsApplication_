f6474a874967e305c850eda71d9f7f7155867ae0
HIDDENPOOL <kazahstanuzbekskij@gmail.com>
2022-10-31 00:43:06 +0300

    UPD 1.6

--- app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
@@ -1,13 +1,19 @@
 package com.example.newsapp.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
 import com.example.newsapp.databinding.FragmentMainBinding
+import com.example.newsapp.ui.adapters.NewsAdapter
+import com.example.newsapp.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,16 +23,58 @@ class MainFragment : Fragment() {
     private val eBinding get() = _binding!!
 
     private val viewModel by viewModels<MainViewModel>()
+    lateinit var newsAdapter: NewsAdapter // создаю адаптер
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        return eBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = bundleOf("article" to it)
+            view.findNavController().navigate(
+                R.id.action_mainFragment_to_detailsFragment,
+                bundle
+            )
+        }
+
+        viewModel.newsLiveData.observe(viewLifecycleOwner) { responce ->
+            when (responce) {
+                // действия взависимости от того что с обёрткой Resource даём действия
+                is Resource.Success -> {
+                    eBinding.pagProgressBar.visibility = View.INVISIBLE
+                    // Загружаем данные в RecyclerView
+                    responce.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    eBinding.pagProgressBar.visibility = View.INVISIBLE
+                    responce.data?.let {
+                        Log.e("TAG 228", "MainFragment:error${it}")
+                    }
+                }
+
+                is Resource.Loading -> {
+                    eBinding.pagProgressBar.visibility = View.VISIBLE
+                }
+
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        newsAdapter = NewsAdapter()
+        eBinding.newsAdapter.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
