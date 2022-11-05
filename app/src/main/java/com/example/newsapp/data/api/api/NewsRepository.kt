7d78cee87606711b2ca7467f300af39faa8a0785
HIDDENPOOL <kazahstanuzbekskij@gmail.com>
2022-11-05 22:41:40 +0300

    UPD 1.7

--- app/src/main/java/com/example/newsapp/data/api/api/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/data/api/api/NewsRepository.kt
@@ -1,8 +1,12 @@
 package com.example.newsapp.data.api.api
 
+import com.example.newsapp.data.api.db.ArticleDao
 import javax.inject.Inject
 
-class NewsRepository @Inject constructor(private val newsService: NewsService) {
+class NewsRepository @Inject constructor(
+    private val newsService: NewsService,
+    private val articleDao: ArticleDao
+) {
     suspend fun getNews(countryCode: String, pageNumber: Int) =
         newsService.getHeadLines(countryCode = countryCode, page = pageNumber)
 }
--- app/src/main/java/com/example/newsapp/di/AppModule.kt
+++ app/src/main/java/com/example/newsapp/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.newsapp.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.newsapp.data.api.api.NewsService
+import com.example.newsapp.data.api.db.ArticleDao
+import com.example.newsapp.data.api.db.ArticleDataBase
 import com.example.newsapp.utils.Constants.Companion.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -37,4 +42,15 @@ object AppModule {
             .client(okHttpClient())
             .build()
             .create(NewsService::class.java)
+
+    fun provideArticleDataBase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(
+            context,
+            ArticleDataBase::class.java, "article_datbase"
+        )
+            .build()
+
+    fun provideArticleDaot(appDataBase: ArticleDataBase): ArticleDao {
+        return appDataBase.getArticleDao()
+    }
 }
